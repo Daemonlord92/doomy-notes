306880bc18b085d2d92d3f497872144c5d0aea61
Matthew Martin <matthew-martin@lambdastudents.com>
2021-07-28 07:14:19 -0500

    Setup JWT Denylist Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable,
-        :validatable
+        :validatable,
+        :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 end
